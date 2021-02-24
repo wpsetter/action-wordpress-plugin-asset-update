2ff6abefd88b72258013e74101ec15227fb802a4
wpsetter <38642203+wpsetter@users.noreply.github.com>
2021-02-24 16:30:22 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -34,7 +34,7 @@ if [[ -z "$README_NAME" ]]; then
 fi
 echo "ℹ︎ README_NAME is $README_NAME"
 
-SVN_URL="https://plugins.svn.wordpress.org/${SLUG}/"
+SVN_URL="https://crush.svn.beanstalkapp.com/${SLUG}/"
 SVN_DIR="/github/svn-${SLUG}"
 
 # Checkout just trunk and assets for efficiency
